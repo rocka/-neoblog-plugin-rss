22a2d53b4e0a6d041af3058101a2c744110e66a5
Rocka <i@rocka.me>
2019-09-14 21:29:42 +0800

    add itemOptions; correct link element and content-type

--- index.js
+++ index.js
@@ -25,16 +25,17 @@ class PluginRSS {
             feed_url: `${options.feedOptions.site_url}${this.route}`,
             generator: 'node-rss'
         }, options.feedOptions)
+        this.itemOptions = options.itemOptions || {};
     }
 
     /**
-     * @see https://tools.ietf.org/html/rfc5005#section-3
+     * @see https://tools.ietf.org/html/rfc5005#appendix-B
      * @param {string} type
      * @param {number} page
      */
     makePaginationLink(type, page) {
         return {
-            link: {
+            'atom:link': {
                 _attr: {
                     rel: type,
                     href: `${this.feedOptions.feed_url}?page=${page}`
@@ -58,9 +59,10 @@ class PluginRSS {
         if (page > 1) {
             custom_elements.push(this.makePaginationLink('previous', page - 1));
         }
-        const feed = new RSS(Object.assign({ custom_elements }, this.feedOptions));
+        const feed = new RSS({ ...this.feedOptions, custom_elements });
         articles.slice(offset, offset + limit).forEach(a => {
             feed.item({
+                ...this.itemOptions,
                 title: a.meta.title,
                 description: a.html,
                 url: `${this.feedOptions.site_url}/article/${a.file.base}`,
@@ -76,7 +78,7 @@ class PluginRSS {
         const router = new Router();
         router.get(this.route, ctx => {
             const page = Number(ctx.query.page || 1)
-            ctx.set('Content-Type', 'text/xml');
+            ctx.set('Content-Type', 'application/rss+xml; charset=utf-8');
             ctx.body = this.getFeed(server.state.articles, this.limit, page);
         });
         server.app.use(router.routes());
